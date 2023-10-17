9c0be20938b4f90c9af3a80a8ce4609bee7a1117
prashant mhatre <prashantmhatre@outlook.in>
2023-10-18 00:26:57 +0530

    #fix

--- src/portfolio/skills.js
+++ src/portfolio/skills.js
@@ -6,10 +6,15 @@ const Skills = ({data}) => {
     <>
         {Object.keys(data).map((key,index)=>(
           <>
-            <div className='flex justify-left items-center pl-0'>
-              <img src={data[key].img} className='w-[15px] h-[15px] mr-2'/><span className='text-[15px] font-bold capitalize'>{key ? key.replaceAll('_', ' ') : ""}</span>
+            <div className='flex justify-left items-start pl-0'>
+              <img src={data[key].img} className='w-[25px] h-[25px] mr-2'/>
+              <div>
+                <div className='text-[15px] font-bold capitalize'>{key ? key.replaceAll('_', ' ') : ""}</div>
+                <div className='leading-[30px]'>
+                  <Capsules data={data[key].tech}/>
+                </div>
+              </div>
             </div>
-            <Capsules data={data[key].tech}/>
           </>        
         ))}
     </>
